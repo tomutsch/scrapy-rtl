3702d805240f2632bc0b9d51e5c97a75bf62976a
Tom Mutsch <tom.mutsch@gmail.com>
2022-02-04 20:14:34 +0100

    remove comments

--- scrapyrtl/spiders/rtllu.py
+++ scrapyrtl/spiders/rtllu.py
@@ -98,9 +98,7 @@ class rtlluSpider(scrapy.Spider):
                     most_read = most_read
                 )
 
-                # print(article)
                 return article
-                # yield article
 
 
     
